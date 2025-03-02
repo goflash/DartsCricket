a59ac5e22cfae0addd4f4a319a6b468c79f78534
Rondo_Twos <didscompany@gmail.com>
2025-03-02 22:22:24 +0300

    It's DartsCricket

--- GoServerAPI.go
+++ GoServerAPI.go
@@ -1,13 +1,13 @@
 package main
- 
+
 import (
-"fmt"
-"net/http"
+	"fmt"
+	"net/http"
 )
- 
+
 func main() {
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintf(w, "Hello World!")
-})
-http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World! It's DartsCricket")
+	})
+	http.ListenAndServe(":80", nil)
 }
